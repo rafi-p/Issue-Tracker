bb123c16bcee71dfe63d62ac1db70ab87a5124ee
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2023-05-22 23:53:04 +0700

    rollback

--- src/helpers/customFetch.ts
+++ src/helpers/customFetch.ts
@@ -1,12 +1,7 @@
-
-import axios from 'axios';
-
 export async function customFetch<T>(url: string, options?: object): Promise<T> {
     const res = await fetch(url, options);
     if (res.ok) {
-        // const result = await res.json();
-        const result = await res.text();
-        console.log(result)
+        const result = await res.json();
 
         if (result.error) {
             throw new Error(result.error);
