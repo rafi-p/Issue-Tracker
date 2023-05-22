151a36ee759453ff313bfbac57eab1fa8b100c78
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2023-05-22 17:30:57 +0700

    Add validation for result so it does not get error when no data

--- src/helpers/customFetch.ts
+++ src/helpers/customFetch.ts
@@ -4,6 +4,7 @@ export async function customFetch<T>(url: string, options?: object): Promise<T> 
     if (res.ok) {
         const result = await res.json();
 
+        if(!result) throw new Error(`No data was found!`);
         if (result.error) {
             throw new Error(result.error);
         }
