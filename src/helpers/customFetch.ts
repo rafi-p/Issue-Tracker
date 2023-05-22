e791863911a28821d49b99ef85919255f8aa66d5
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2023-05-22 23:47:51 +0700

    debuging with text() on response

--- src/helpers/customFetch.ts
+++ src/helpers/customFetch.ts
@@ -2,30 +2,16 @@
 import axios from 'axios';
 
 export async function customFetch<T>(url: string, options?: object): Promise<T> {
-    // const res = await fetch(url, options);
-    // if (res.ok) {
-    //     const result = await res.json();
+    const res = await fetch(url, options);
+    if (res.ok) {
+        // const result = await res.json();
+        const result = await res.text();
+        console.log(result)
 
-    //     if (result.error) {
-    //         throw new Error(result.error);
-    //     }
-    //     return result;
-    // }
-    // throw new Error(`Error ${res.status}: ${res.statusText}`);
-
-    // const res = await fetch(url, options);
-    let custom = {...options}
-    if(options?.body) {
-        custom.data = options?.body
-        delete custom["body"]
-    }
-    const res = await axios({
-        url,
-        ...custom
-    })
-
-    if (res.status === 200) {
-        return res.data;
+        if (result.error) {
+            throw new Error(result.error);
+        }
+        return result;
     }
     throw new Error(`Error ${res.status}: ${res.statusText}`);
 }
