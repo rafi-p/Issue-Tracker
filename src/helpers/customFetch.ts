bcdf68034c451db9fbd6cc3d75070f5d29215c00
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2023-05-22 18:39:29 +0700

    add log to fetch to see from deployed

--- src/helpers/customFetch.ts
+++ src/helpers/customFetch.ts
@@ -3,8 +3,13 @@ export async function customFetch<T>(url: string, options?: object): Promise<T> 
     const res = await fetch(url, options);
     if (res.ok) {
         console.log(res, 'before result')
-        const result = await res.json();
-        console.log(result, 'after result') //to see from vercel
+        const textJSON = await res.text();
+        const result = JSON.parse(textJSON)
+
+        console.log(textJSON, result, 'after result') //to see from vercel
+        // const tes = await res.text()
+        // console.log(tes,  'tes result') //to see from vercel
+
 
         if (result.error) {
             throw new Error(result.error);
